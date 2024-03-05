33eb51e2ddc72db8a152302dc98747507c2fc5af
ARNAB BASAK <basakarnab912@gmail.com>
2024-03-05 20:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import string
 import pickle
 import streamlit as st
 import nltk
-nltk.data.path.append("C:\\Users\\arnab\\AppData\\Roaming\\nltk_data\\tokenizers\\punkt\\PY3")
+# nltk.data.path.append("C:\\Users\\arnab\\AppData\\Roaming\\nltk_data\\tokenizers\\punkt\\PY3")
 from nltk.stem.porter import PorterStemmer
 from nltk import word_tokenize
 from nltk.corpus import stopwords
@@ -29,13 +29,14 @@ model = pickle.load(open('model.pkl','rb'))
 
 def predict_news_authenticity(news_text):
     preprocessed_news_text = news_text
-    prediction = model.predict([preprocessed_news_text])
+    # prediction = model.predict([preprocessed_news_text])
+    prediction = model.decision_function([preprocessed_news_text])
 
-    if prediction == 0:
-        return "Fake"
-    else:
-        return "Real"
-
+    # if prediction == 0:
+    #     return "Fake"
+    # else:
+    #     return "Real"
+    return prediction
 st.title("Fake News Detection")
 
 input_news = st.text_area("Enter the News")
@@ -48,13 +49,13 @@ if col1.button('Predict'):
     if not input_news:
         st.header("Please enter a News first!!!")
     else:
-        #  preprocess
         result = predict_news_authenticity(input_news)
-        #  Display
-        if result == "Fake":
-            st.header("The news is Fake")
-        else:
-            st.header("The news is Real")
+        # if result == "Fake":
+        #     st.header("The news is Fake")
+        # else:
+        #     st.header("The news is Real")
+
+        st.header("Result is "+result)
 
 # Place button in the second column
 if col2.button('Clear Result'):
