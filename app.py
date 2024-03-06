a18908d6210425c2a9c78018f6fc3bdd6d07afd0
ARNAB BASAK <basakarnab912@gmail.com>
2024-03-06 20:04:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ col1, col2 = st.columns(2)
 
 def animate_prediction(real,fake):
     progress_placeholder = st.empty()
-    st.header("Calculating....")
+    calculation_text = st.text_input("Calculating....", "", key="calculation_text")
     for i in range(1, real):
         html_code = f"""
         <style>
@@ -83,8 +83,7 @@ def animate_prediction(real,fake):
         progress_placeholder.write(html_code, unsafe_allow_html=True)
         time.sleep(0.1)  
         
-    progress_placeholder.empty()
-    st.header("Result -")
+    st.session_state.calculation_text = ""
     html = f'<div style="display: flex; justify-content: space-between;">'
     html += f'<div style="font-weight: bold">Real - {real}%</div>'
     html += f'<div style="font-weight: bold">Fake - {fake}%</div>'
